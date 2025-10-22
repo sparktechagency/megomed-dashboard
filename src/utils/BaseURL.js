02b61076cf7951ef0c7be55e21a80a3b510f9438
pranab56 <pronabhalder53@gmail.com>
2025-10-22 17:29:52 +0600

    all ok

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,4 +1,4 @@
-export const baseURL = "http://10.10.7.65:5006/api/v1";
-// export const baseURL = "http://82.165.134.157:5000/api/v1";
-// export const baseURLImage = "http://10.10.7.107:5006/";
-export const baseURLImage = "http://10.10.7.65:5006/";
+// export const baseURL = "http://10.10.7.65:5006/api/v1";
+export const baseURL = "http://82.165.134.157:5000/api/v1";
+// export const baseURLImage = "http://10.10.7.65:5006/";
+export const baseURLImage = "http://82.165.134.157:5000";
