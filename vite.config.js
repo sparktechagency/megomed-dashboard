9ad8ec2dbbce6b689b5a215ac3db58348485747b
pranab56 <pronabhalder53@gmail.com>
2025-11-20 11:23:19 +0600

    vit config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,32 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
 
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     // host: "31.97.98.240",
+//     host: "0.0.0.0",
+//     // port: 4174,
+//   },
+//   build: {
+//     sourcemap: false, // disables source maps for production
+//     minify: "esbuild", // faster minifier
+//   },
+// });
+
+
+
+
+// vite.config.js
+import { defineConfig } from 'vite'
+ 
 export default defineConfig({
-  plugins: [react()],
   server: {
-    // host: "31.97.98.240",
-    host: "0.0.0.0",
-    // port: 4174,
+    host: true,        // listen on 0.0.0.0 so nginx can reach it
+    port: 3001
   },
-  build: {
-    sourcemap: false, // disables source maps for production
-    minify: "esbuild", // faster minifier
-  },
-});
+  // optionally also allow preview host if you sometimes run `vite preview`
+  preview: {
+    allowedHosts: ['dashboard.lunq.fr', 'www.dashboard.lunq.fr']
+  }
+})
