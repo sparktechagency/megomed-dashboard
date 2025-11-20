2a1ac805da2b035f527d05ef2d6aeeeb11dde5df
pranab56 <pronabhalder53@gmail.com>
2025-11-20 14:39:25 +0600

    all ok

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,5 +1,5 @@
 // export const baseURL = "http://10.10.7.65:5006/api/v1";
-export const baseURL = "https://api.lunq.fr";
+export const baseURL = "https://api.lunq.fr/api/v1";
 // export const baseURLImage = "http://10.10.7.65:5006/";
 // export const baseURLImage = "http://82.165.134.157:5000";
 
